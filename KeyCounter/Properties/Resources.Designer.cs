725ceb3e4c4e5adfcf265daa8230befab84e8079
mibbio <contact@mibbiodev.de>
2018-01-20 13:23:06 +0100

    UI layout for font settings

--- KeyCounter/Properties/Resources.Designer.cs
+++ KeyCounter/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace KeyCounter.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -138,6 +138,15 @@ namespace KeyCounter.Properties {
         public static string TXT_HEADING_COLOR {
             get {
                 return ResourceManager.GetString("TXT_HEADING_COLOR", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Font Settings.
+        /// </summary>
+        public static string TXT_HEADING_FONT {
+            get {
+                return ResourceManager.GetString("TXT_HEADING_FONT", resourceCulture);
             }
         }
         
